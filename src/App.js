773c1707f5db3277fa3b09f094aecdbbc694a8c1
RoyerMateu <rogermateu2001@gmail.com>
2022-02-16 13:17:18 +0100

    styles finish (almost)

--- src/App.js
+++ src/App.js
@@ -12,18 +12,28 @@ function App() {
         <h1>T O D O</h1>
         <div className="main-add-list">
           <div>
-            <h2>Hola</h2>
+            <input type="text" />
           </div>
         </div>
         <div className="main-item-list">
           <ul>
-            <li className="list-group-item list-group-item-action li">Hola</li>
-            <li className="list-group-item list-group-item-action li">Hola</li>
-            <li className="list-group-item list-group-item-action li">Hola</li>
-            <li className="list-group-item list-group-item-action li">Hola</li>
+            <li className="list-group-item li">Hola</li>
+            <li className="list-group-item li">Hola</li>
+            <li className="list-group-item li">Hola</li>
+            <li className="list-group-item li">Hola</li>
+            <nav className="list-group-item li-nav">
+              <ul>
+                <li className="nav-li-first">5 items left</li>
+                <li className="nav-li">All</li>
+                <li className="nav-li">Active</li>
+                <li className="nav-li">Completed</li>
+                <li className="nav-li-last">Clear Completed</li>
+              </ul>
+            </nav>
           </ul>
         </div>
       </section>
+      <footer>Drag and drop to reorder list</footer>
     </>
   );
 }
