beda89fc24d5800e237f2f53cd4dc99526471d35
ibaialberdi <ibaialberdi@gmail.com>
2022-02-21 11:30:29 +0100

    minor fixes

--- src/pages/Active.js
+++ src/pages/Active.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ListTodo from "../components/ListTodo";
 
-function Active({ todos, toogleItemTodo }) {
+function Active({ todos, toogleItemTodo, removeTodo }) {
   const activeTodos = todos.filter((item) => {
     return !item.done;
   });
   return (
     <div>
-      <ListTodo todos={activeTodos} toogleItemTodo={toogleItemTodo} />
+      <ListTodo
+        todos={activeTodos}
+        toogleItemTodo={toogleItemTodo}
+        removeTodo={removeTodo}
+      />
     </div>
   );
 }
--- src/pages/Completed.js
+++ src/pages/Completed.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ListTodo from "../components/ListTodo";
 
-function Completed({ todos, toogleItemTodo }) {
+function Completed({ todos, toogleItemTodo, removeTodo }) {
   const completedTodos = todos.filter((item) => {
     return item.done;
   });
   return (
     <div>
-      <ListTodo todos={completedTodos} toogleItemTodo={toogleItemTodo} />
+      <ListTodo
+        todos={completedTodos}
+        toogleItemTodo={toogleItemTodo}
+        removeTodo={removeTodo}
+      />
     </div>
   );
 }
